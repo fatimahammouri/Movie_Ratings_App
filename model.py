5fa7df6ed028d026fa05673b678ebd3e001882b9
Fatima <fatima.hammouri@gmail.com>
2021-03-31 13:35:19 +0000

    created Movie class that creates a movies table in the db

--- model.py
+++ model.py
@@ -15,6 +15,18 @@ class User(db.Model):
     def __repr__(self):
         return f"User user_id={self.user_id} email={self.email}"
 
+class Movie(db.Model):
+    __tablename__ = "movies"
+    movie_id = db.Column(db.Integer, primary_key=True,
+                        autoincrementing=True)
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Movie movie_id={self.movie_id} title={self.title}>'
+        
 
 def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
