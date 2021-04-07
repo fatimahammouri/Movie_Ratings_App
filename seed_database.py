4e06641e8eb0a35ae4f16f3ea3fbfe75a87c934b
Fatima <fatima.hammouri@gmail.com>
2021-04-07 16:40:14 +0000

    created 10 users and ratings by calling create_user/rating from crud

--- seed_database.py
+++ seed_database.py
@@ -40,3 +40,24 @@ for movie in movie_data:
 
     db_movie = crud.create_movie(title, overview,release_date,poster_path)
     movies_in_db.append(db_movie)
+
+
+# we need to create randomly numbered Users 
+# Sooo.. we'll just loop over 10 to create 10 users
+# generate random Emails and passwords
+# create users by calling create_user func from crud
+for n in range(10):
+    email = f'user{n}@testing.com'
+    password = f'test{n}'
+
+    user = crud.create_user(email, password)
+
+# we need to create 10 random ratings as well
+# Soo... we'll choose a random movie, a random score
+# create a rating by calling create_rating func from crud 
+for _ in range(10):
+    random_movie = choice(movies_in_db)
+    score = randint(1, 5)
+
+    rating = crud.create_rating(user, random_movie, score)
+
